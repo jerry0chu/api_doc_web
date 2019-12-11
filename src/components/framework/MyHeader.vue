4f8c7126f6d1d49d148476e32aa30ad2d037d311
jerry0chu <1052757325@qq.com>
2019-12-11 20:51:10 +0800

    add download project button

--- src/components/framework/MyHeader.vue
+++ src/components/framework/MyHeader.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
   <div>
     <a-row :gutter="16">
       <a-col :span="2">
@@ -7,7 +7,25 @@
       <a-col :span="4">
         <a-button @click="gotToProject" style="margin-left: 20px">Project Home</a-button>
       </a-col>
+      <a-col :span="1" :offset="15">
+        <a-button type="primary" shape="circle" icon="download" @click="downloadVisible=true"/>
+      </a-col>
     </a-row>
+    <a-modal title="Choose Your Framework" :width="650" v-model="downloadVisible" :footer="null">
+      <a-row>
+        <a-col :span="8">
+          <a-button type="primary" round icon="download" @click="handleDownloadClick('falsk')">Flask Project</a-button>
+        </a-col>
+        <a-col :span="8">
+          <a-button type="primary" round icon="download" @click="handleDownloadClick('springboot')">SpringBoot Project
+          </a-button>
+        </a-col>
+        <a-col :span="8">
+          <a-button type="primary" round icon="download" @click="handleDownloadClick('scalatra')">Scalatra Project
+          </a-button>
+        </a-col>
+      </a-row>
+    </a-modal>
   </div>
 </template>
 
@@ -16,15 +34,20 @@
     name: "myheader",
     data()
     {
-        return{
-
-        }
+      return {
+        downloadVisible: false
+      }
     },
     methods: {
       gotToProject()
       {
         this.$router.push("/")
       },
+      handleDownloadClick(projectType)
+      {
+        console.log("hello woerl", projectType)
+        this.downloadVisible = false
+      }
     }
   }
 </script>
